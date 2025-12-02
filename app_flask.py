59d6e966e6fa8ea7dd5de6c98aebc08fe1872e8a
vivi 3 <vitoriagcavalcantesales@gmail.com>
2025-12-02 01:53:21 -0300

    adicionando front

--- app_flask.py
+++ app_flask.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 DATABASE_NAME = 'biblioteca.db'
 
@@ -13,22 +14,29 @@ def get_db_connection():
 #rota (GET para listar/) (POST para adicionar) livros
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    #LÓGICA DO POST ADICIONAR LIVRO
     if request.method == 'POST':
         titulo = request.form['titulo']
         autor = request.form['autor']
         ano_publicacao = request.form['ano_publicacao']
+        try:
+            ano_publicacao = int(request.form['ano_publicacao'])
+        except ValueError:
+            ano_publicacao = None
+
         disponivel = 1 if 'disponivel' in request.form else 0
-        
+
         conn = get_db_connection()
         conn.execute('INSERT INTO livros (titulo, autor, ano_publicacao, disponivel) VALUES (?, ?, ?, ?)',
                      (titulo, autor, ano_publicacao, disponivel))
         conn.commit()
         conn.close()
-    return redirect(url_for('index'))
 
-    # Listar livros
+        return redirect(url_for('index'))
+
+    # LÓGICA DO GET Listar livros
     conn = get_db_connection()
-    livros = conn.execute('SELECT * FROM livros').fetchall()
+    livros = conn.execute('SELECT * FROM livros ORDER BY id DESC').fetchall()
     conn.close()
     
     # Renderiza o template HTML 
